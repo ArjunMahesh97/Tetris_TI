e16a837509e35a1d776f97022723077c6044c8a0
Arjun Devappa <arjunmahesh97@gmail.com>
2020-08-30 03:40:07 +0530

    Vertical default and fast movement

--- Assets/BlockMovement.cs
+++ Assets/BlockMovement.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class BlockMovement : MonoBehaviour
 {
+    float prevTime;
+    [SerializeField] float fallTimeGap = 1f;
+    float fallTimeGapFactor;
+    
+
+    [SerializeField] float downKeySpeedFactor=0.2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +27,21 @@ public class BlockMovement : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.D))
         {
             transform.position += new Vector3(1, 0, 0);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            fallTimeGapFactor = fallTimeGap * downKeySpeedFactor;
+        }
+        else
+        {
+            fallTimeGapFactor = fallTimeGap;
+        }
+
+        if(Time.time-prevTime>fallTimeGapFactor)
+        {
+            transform.position += new Vector3(0, -1, 0);
+            prevTime = Time.time;
         }
     }
 }
